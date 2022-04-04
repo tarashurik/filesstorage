3a4511bd7e967491e9f036fbf77045d5a1a37f52
tarashurik@mail.ru <tarashurik@mail.ru>
2022-04-04 17:11:41 +0300

    set default values first_name and last_name as None in User schema

--- app/schemas.py
+++ app/schemas.py
@@ -7,8 +7,8 @@ from uuid import UUID
 class UserBase(BaseModel):
     username: str
     email: str
-    first_name: str = None
-    last_name: str = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
 
 
 class UserCreate(UserBase):
