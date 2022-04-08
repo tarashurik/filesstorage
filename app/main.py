92b588e05286b75b5242125f1304078987970dc7
tarashurik@mail.ru <tarashurik@mail.ru>
2022-04-08 15:15:29 +0300

    add reconnecting to DB if it was unsuccessfull

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
 # import uvicorn
+import time
+
 from fastapi import FastAPI
+from sqlalchemy.exc import OperationalError
 
 from database import Base, engine
 from routers import users_router, files_router
 
-
-Base.metadata.create_all(bind=engine)
+while True:
+    try:
+        Base.metadata.create_all(bind=engine)
+        break
+    except OperationalError:
+        print("DataBase hasn't ready yet, try again in 1 second")
+        time.sleep(1)
+        continue
 
 app = FastAPI()
 app.include_router(users_router)
